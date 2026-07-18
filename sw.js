c6f6d2c75ddb64b09bb0bf522931f91e1ce44245
davidmcguire47-create <davidmcguire47@gmail.com>
2026-07-19 00:58:16 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,23 @@
-const CACHE_NAME = 'accent-gaming-v2';
-const urlsToCache = [ '/', '/index.html', '/manifest.json' ];
+const CACHE_NAME = 'accent-gaming-v1';
+const ASSETS = [
+  '/Accent-Gaming-App/',
+  '/Accent-Gaming-App/index.html',
+  '/Accent-Gaming-App/manifest.json',
+  '/Accent-Gaming-App/image_3fb224.jpg'
+];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
